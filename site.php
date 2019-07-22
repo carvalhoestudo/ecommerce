9ceeeb5e6b97e1abfcad591aa09629961dec1599
carvalhoestudo <carvalho.madureira@hotmail.com>
2019-07-22 14:13:37 -0300

    Minha conta

--- site.php
+++ site.php
@@ -36,6 +36,7 @@ $app->get("/categories/:idcategory", function($idcategory){
 			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
 			'page'=>$i
 		]);
+
 	}
 
 	$page = new Page();
@@ -306,4 +307,64 @@ $app->post("/forgot/reset", function(){
 	$page = new Page();
 	
 	$page->setTpl("forgot-reset-success");	
+});
+
+$app->get("/profile", function(){
+
+	User::verifyLogin(false);
+
+	$user = User::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("profile", [
+		'user'=>$user->getValues(),
+		'profileMsg'=>User::getSuccess(),
+		'profileError'=>User::getError()
+	]);
+
+});
+
+$app->post("/profile", function(){
+
+	User::verifyLogin(false);
+
+	if(!isset($_POST['desperson']) || $_POST['desperson'] === ''){
+		User::setError("Preencha o campo nome.");
+		header('Location: /profile');
+		exit;
+	}
+
+	if(!isset($_POST['desemail']) || $_POST['desemail'] === ''){
+		User::setError("Preencha o campo email.");
+		header('Location: /profile');
+		exit;
+	}
+
+	$user = User::getFromSession();
+
+	if ($_POST['desemail'] !== $user->getdesemail()) {
+		
+		if (User::checkLoginExists($_POST['desemail']) === true) {
+
+			User::setError("Este email já está em uso, tente cadastrar outro email!");
+			header('Location: /profile');
+			exit;
+		}
+
+	}
+
+	$_POST['inadmin'] = $user->getinadmin();
+	$_POST['despassword'] = $user->getdespassword();
+	$_POST['deslogin'] = $_POST['desemail'];
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	User::setSuccess("Dados alterados com sucesso!");
+
+	header('Location: /profile');
+	exit;
+
 });
